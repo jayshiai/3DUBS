5d3ed98ff38308e698066908ecfbef25a924c59c
jayshiai <jayvardhanpatil7811@gmail.com>
2024-03-07 23:36:02 +0530

    MORE ERRORS

--- src/components/contactus/ContactPage.jsx
+++ src/components/contactus/ContactPage.jsx
@@ -1,37 +1,13 @@
 
 "use client"
-
-import { useEffect, useState } from 'react';
-import ContactCard from "@/components/contactus/ContactCard"
-import ContactClock from '@/components/contactus/ContactClock';
+import ContactCard from "./ContactCard"
+import ContactClock from './ContactClock';
 
 const ContactPage = () => {
-    // Define your variants
-    const variants = [<ContactCard key={0} />, <ContactClock key={1} />];
-
-    // State to hold the selected variant index
-    const [variantIndex, setVariantIndex] = useState();
-
-    useEffect(() => {
-        // Check if config exists in Session Storage
-        const configString = sessionStorage.getItem('config');
-
-        if (configString) {
-            // Parse the config JSON string
-            const config = JSON.parse(configString);
-
-            // Update the variant index based on the 'about' key in the config
-            setVariantIndex(config['contact']);
-        }
-        else {
-            setVariantIndex(0)
-        }
-    }, []); // Empty dependency array ensures this effect runs only once on component mount
-
     return (
         <>
-
-            {variants[variantIndex]}
+            <ContactCard />
+            <ContactClock />
         </>
     )
 }
